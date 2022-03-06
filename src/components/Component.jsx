40e0732943018cc00a468a311c36a492eafb318c
LeviRomans <40896186+LeviRomans@users.noreply.github.com>
2022-03-06 16:08:49 -0500

    emojipedia style updated

--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -2,16 +2,14 @@ import React from "react";
 
 function Component(props) {
   return (
-    <div>
-      <div className="term">
-        <dt>
-          <span className="emoji" role="img" aria-label={props.name}>
-            {props.emoji}
-          </span>
-          <span>{props.name}</span>
-        </dt>
-        <dd>{props.meaning}</dd>
-      </div>
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label={props.name}>
+          {props.emoji}
+        </span>
+        <span>{props.name}</span>
+      </dt>
+      <dd>{props.meaning}</dd>
     </div>
   );
 }
